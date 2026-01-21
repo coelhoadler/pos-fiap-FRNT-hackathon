b4556e7da651bee8820c3b89ba6c07c22a140bf8
Adler Santos <adlercoelhosantos12@gmail.com>
2026-01-20 21:22:12 -0300

    refactor: restructure app layout and remove unused components
    - Deleted the HomeScreen component and related files to streamline the app.
    - Introduced a new LoginScreen component for user authentication.
    - Updated the RootLayout to utilize a stack navigator for navigation.
    - Removed modal screen and external link components as they were not in use.
    - Cleaned up unused hooks and theme-related files to reduce complexity.
    - Updated firebaseAuth service to specify return type for signUp function.
    - Removed reset-project script as it was no longer necessary.
    - Updated package dependencies for navigation libraries.

---
+++ mobile/mindEase/app/screens/login/index.tsx
@@ -0,0 +1,16 @@
+import { Text, TextInput, View } from "react-native";
+
+export default function LoginScreen() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Edit o arquivo login da pasta de Screens</Text>
+            <TextInput>123</TextInput>
+        </View>
+    );
+}
