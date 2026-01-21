b6c257f52d27455211f2bc1423b9b24acf3de6b7
Adler Santos <adlercoelhosantos12@gmail.com>
2026-01-21 20:53:32 -0300

    feat(register): refactor RegisterScreen to use SafeAreaView and dynamic theming

--- mobile/mindEase/app/(screens)/register/register.tsx
+++ mobile/mindEase/app/(screens)/register/register.tsx
@@ -1,17 +1,19 @@
-import { Text, TextInput, View } from "react-native";
+import { useColorScheme } from "@/app/hooks/use-color-scheme.web";
+import { Text, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./styles";
 
 export default function RegisterScreen() {
 
+    const colorSchemeRaw = useColorScheme();
+    const colorScheme: 'light' | 'dark' | undefined = colorSchemeRaw ?? 'dark';
+
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
+        <SafeAreaView
+            style={styles(colorScheme).container}
         >
             <Text>Criar conta</Text>
             <TextInput>123</TextInput>
-        </View>
+        </SafeAreaView>
     );
 }
