f5836a9b9c074a5211c680be7bc3f260cc9c9a26
Erick <erick1661@gmail.com>
2026-02-04 23:02:47 -0300

    modal de legenda

--- mobile/app/components/ui/legend/legendContent/styles.ts
+++ mobile/app/components/ui/legend/legendContent/styles.ts
@@ -6,19 +6,40 @@ export function createStyles(colorScheme: "light" | "dark" = "dark") {
 
   return StyleSheet.create({
     legendContent: {
-
+      width: '100%',
+      minWidth:'100%',
     },
     subtitle: {
-
+      width: '100%',
+      marginBottom: 20,
+      fontSize: 18,
+      fontWeight: '500',
+      textAlign:'left',
+      color: colors.text
     },
-    wrapperItems: {
-
+    wrapperItem: {
+      display: 'flex',
+      flexDirection: 'row',
+      width: '100%',
+      justifyContent:'flex-start',
+      gap: 10,
+      marginBottom: 10,
+      padding: 10,
+      backgroundColor:colors.colorSuccess,
+      borderRadius:5,
     },
     wrapperIcon: {
-
+      width: '6%',
+      marginTop:1
     },
     descriptionItem: {
-
+      fontSize: 16,
+      fontWeight: '400',
+      color: colors.text,
+      textAlign:'left',
+      width: '94%',
+      maxWidth:'94%',
+      paddingRight:5
     },
   });
 }
--- mobile/app/interface/modal/index.ts
+++ mobile/app/interface/modal/index.ts
@@ -35,4 +35,5 @@ export interface ILegendContent{
 export interface ILegendContentItem{
     description:string;
     icon?: React.ReactElement;
+    styleLegendIcon?: StyleProp<ViewStyle>;
 }
