82a7b647d589f9b6d3794213987fedc7bda5b1ab
Adler Santos <adlercoelhosantos12@gmail.com>
2026-02-12 19:50:21 -0300

    refactor(home-screen): remove ParallaxScrollView and simplify layout structure

--- mobile/app/(screens)/home/(tabs)/index.tsx
+++ mobile/app/(screens)/home/(tabs)/index.tsx
@@ -15,19 +15,7 @@ export default function HomeScreen() {
   });
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Boas Vindas!</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ThemedView style={{ flex: 1 }}></ThemedView>
   );
 }
 
